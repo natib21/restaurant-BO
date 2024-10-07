8d7a892c9153a0f7268ea5b7c71acd63e56cddb1
nathnael zelalem <nathnaelzelalem@gmail.com>
2024-10-07 05:22:07 -0700

    add Filtering to menu

--- controllers/menuController.js
+++ controllers/menuController.js
@@ -2,7 +2,21 @@ const Menu = require('../models/menuModel');
 
 exports.getAllMenu = async (req, res) => {
   try {
-    const allMenu = await Menu.find();
+    const queryObj = { ...req.query };
+
+    const excludedFields = ['page', 'sort', 'limit', 'fields'];
+
+    excludedFields.forEach((el) => delete queryObj[el]);
+
+    let queryStr = JSON.stringify(queryObj);
+
+    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
+
+    console.log(JSON.parse(queryStr));
+
+    const query = Menu.find(JSON.parse(queryStr));
+
+    const allMenu = await query;
 
     res.status(200).json({
       status: 'success',
--- models/menuModel.js
+++ models/menuModel.js
@@ -8,24 +8,42 @@ const menuSchema = mongoose.Schema({
   name: {
     type: String,
     required: [true, 'Name Must Be State'],
+    unique: true,
   },
   description: String,
   price: {
     type: Number,
     required: [true, 'Price Must be set'],
+    default: 0,
   },
   available: {
     type: Boolean,
     required: [true, 'Availability Must Be Stated'],
+    default: true,
   },
   ingredients: [String],
-  categories: String,
-  image: String,
+  category: String,
+  image: {
+    type: String,
+    required: [true, 'Food Must Have An Image'],
+  },
   prep_time: {
     type: String,
     required: [true, 'Time Must Be Included'],
+    default: '20 min',
   },
-  sizes: { type: [sizeSchema], required: true },
+  sizes: {
+    type: [sizeSchema],
+    required: true,
+  },
+  averageRating: {
+    type: Number,
+    default: 0,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+  },
 });
 
 const Menu = mongoose.model('Menu', menuSchema);
