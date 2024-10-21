e4c31dfd941bdec7c5ebe738e2d0c57d66963e94
nathnael zelalem <nathnaelzelalem@gmail.com>
2024-10-21 08:51:17 -0700

    adding user model with authControl

--- models/userModel.js
+++ models/userModel.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
+const bcrypt = require('bcryptjs');
 const userSchema = new mongoose.Schema({
   name: {
     type: String,
@@ -25,6 +26,12 @@ const userSchema = new mongoose.Schema({
   passwordConfirm: {
     type: String,
     required: [true, 'Please provide a password'],
+    validate: {
+      validator: function (el) {
+        return el === this.password;
+      },
+      message: 'password are not the same',
+    },
   },
   role: {
     type: String,
@@ -48,6 +55,14 @@ const userSchema = new mongoose.Schema({
   photo: String,
 });
 
+userSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) return next();
+
+  this.password = await bcrypt.hash(this.password, 12);
+
+  this.passwordConfirm = undefined;
+  next();
+});
 const User = mongoose.model('user', userSchema);
 
 module.exports = User;
