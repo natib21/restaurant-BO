fa4fd9d8c91eafcaeb42ae4436e77a8daaef887f
nathnael zelalem <nathnaelzelalem@gmail.com>
2024-12-19 05:45:02 -0800

    socket io integration

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -1,19 +1,27 @@
 const catchAsync = require('../utils/catchAsync');
+const ApiFeatures = require('../utils/apiFeatures');
 const Order = require('../models/orderModel');
-exports.getAllOrder = (req, res) => {
-  res.status(500).json({
-    status: 'Error',
-    message: 'This route is not defined',
-  });
-};
-exports.createNewOrder = catchAsync(async (req, res, next) => {
-  // Only if items is coming in as a string
-  /*  if (typeof req.body.items === 'string') {
-    req.body.items = JSON.parse(req.body.items);
-  } */
+const { getIo } = require('../socket');
 
-  console.log(req.body);
+exports.getAllOrder = catchAsync(async (req, res) => {
+  const features = new ApiFeatures(Order.find(), req.query)
+    .filter()
+    .sort()
+    .limitFields()
+    .paginate();
+
+  const allOrders = await features.query;
+
+  res.status(200).json({
+    status: 'success',
+    result: allOrders.length,
+    order: allOrders,
+  });
+});
+exports.createNewOrder = catchAsync(async (req, res, next) => {
   const newOrder = await Order.create(req.body);
+  const io = getIo();
+  io.emit('new-order', newOrder);
   res.status(201).json({
     status: 'success',
     order: newOrder,
@@ -25,10 +33,19 @@ exports.getOrder = (req, res) => {
     message: 'This route is not defined',
   });
 };
-exports.updateOrder = (req, res) => {
-  res.status(500).json({
-    status: 'Error',
-    message: 'This route is not defined',
+exports.updateOrder = async (req, res, next) => {
+  const updatedOrder = await Order.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+  });
+
+  // Emit the updated order event
+  io.emit('update-order', updatedOrder);
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      order: updatedOrder,
+    },
   });
 };
 exports.deleteOrder = (req, res) => {
@@ -36,4 +53,24 @@ exports.deleteOrder = (req, res) => {
     status: 'Error',
     message: 'This route is not defined',
   });
+};
+
+exports.acceptOrder = async (req, res, next) => {
+  try {
+    const order = await Order.findById(req.params.id);
+    order.status = 'Accepted';
+    await order.save();
+
+    // Emit the order acceptance event
+    io.emit('accept-order', order._id);
+
+    res.status(200).json({
+      status: 'success',
+      data: {
+        order,
+      },
+    });
+  } catch (err) {
+    next(err);
+  }
 };
