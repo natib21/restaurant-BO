b70d2cb423da51483dae1adf4f45490b888e144f
nathnael <nathnaelzelalem@gmail.com>
2026-01-30 16:54:36 +0300

    update on the cookies

--- app.js
+++ app.js
@@ -43,6 +43,7 @@ const allowedOrigins = [
   'http://127.0.0.1:5173',
   'http://localhost:5174',
   'http://127.0.0.1:5174',
+  'https://restaurant-bo.onrender.com'
   // Add your production frontend URL later, e.g.:
   // 'https://yourapp.com',
   // 'https://www.yourapp.com',
--- controllers/authController.js
+++ controllers/authController.js
@@ -54,7 +54,7 @@ const createSendToken = (user, statusCode, res) => {
   const cookieOptions = {
     expires: new Date(Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000),
     httpOnly: true, // Prevents XSS attacks (JS can't access cookie)
-    sameSite: 'strict', // Protects against CSRF
+    sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax',
     path: '/',
   };
 
